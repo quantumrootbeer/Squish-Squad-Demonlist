2b81479be8711507586351e81821fd53af8112b5
GamerAJG2012 <ggamerajg12@gmail.com>
2025-10-06 15:12:47 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,25 +100,25 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed)
+                        no hack
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        
                     </p>
                 </div>
             </div>
