c3648497d0387858a3849bcd10c05cfe9b8ceb1a
quantumrootbeer <quantumrootbeer@gmail.com>
2026-03-30 20:14:33 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -19,5 +19,36 @@ export function score(rank, percent, minPercent) {
     }
 
     // Old formula
+    /*
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
+    // New formula
+    let score = (-50*Math.pow(rank-1, 0.4) + 350) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
+}
+
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
+}
